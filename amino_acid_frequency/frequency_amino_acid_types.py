834c0eac12cd11ea23eb2a455455e84e1eb7d418
davarm <david.armstrong@uqconnect.edu.au>
2017-01-10 13:51:50 +1000

    working with functional graphs for amino acid frequency

--- amino_acid_frequency/frequency_amino_acid_types.py
+++ amino_acid_frequency/frequency_amino_acid_types.py
@@ -36,11 +36,16 @@ vdw_volume['W']=163
 vdw_volume['Y']=141
 vdw_volume['V']=105
 
-vdw_volume_list=['67','148','96','91','86','114','109','48','118','124','135','90','73','93','163','141','105']
 
-amino_acids=['A','R','N','D','C','c','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V']
+amino_acids=['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V']
+
+#------------------------------------#------------------------------------
+#NATIVE AMINO ACID FREQUENCY
+#------------------------------------#------------------------------------
+native_amino_freq=[0.0892,0.0566,0.0395,0.0542,0.0126,0.0384,0.0615,0.0716,0.0222,0.057,0.0983,0.0506,0.024,0.0393,0.0487,0.0681,0.0559,0.013,0.0295,0.0681]
 
 
+print len(native_amino_freq)
 before_residue_list=[]
 after_residue_list=[]
 get=open('./../'+'angles_secondary_structure.txt','r')
@@ -58,7 +63,7 @@ for line in get:
 
 
 	#------------------------------------#------------------------------------
-	#Calculating the VDW volume of neighbouring amino acids
+	#Appending amino acid length
 	#------------------------------------#---------------------------------
 	"""Defining the neighbouring amino acids of the two Cys residues of a disulfide bond"""
 	neighbouring_aminoacids1=[lines[37]] + [lines[47]] 
@@ -78,14 +83,28 @@ for line in get:
 	after_residue_list.append(cys1_after)
 	after_residue_list.append(cys2_after)
 
+#print before_residue_list
+#print after_residue_list
+before_residue_list=['C' if x=='c' else x for x in before_residue_list]
+after_residue_list=['C' if x=='c' else x for x in after_residue_list]
+
+
+#------------------------------------#------------------------------------
+#The total is the length of the residue list, or the total number of neighbouring residues 
+#Used to then divide the number of each occurence to find the frequency
+#------------------------------------#------------------------------------
+
 total=len(before_residue_list)
 counts_before=Counter(before_residue_list)
 counts_after=Counter(after_residue_list)
+
+
 for key, value in counts_before.items():
 	x=float(value) / float(total)
 	counts_before[key] = x
 for key, value in counts_after.items():
     counts_after[key] = float(value) / float(total)
+
 
 
 y_list_before=[]
@@ -94,10 +113,24 @@ for value in amino_acids:
 	y_list_before.append((counts_before[value]))
 	y_list_after.append(counts_after[value])
 
-print y_list_after
-x_range=range(len(amino_acids))
+print len(y_list_before)
+x_range=range(1,21)
+print len(x_range)
 print x_range
-p = figure(plot_width=400, plot_height=400)
-p.multi_line([x_range,x_range],[y_list_before,y_list_after],color=["firebrick", "navy"], alpha=[0.8, 0.3], line_width=4)
+p = figure(plot_width=600, plot_height=400,x_range=amino_acids)
+#p.multi_line([x_range,x_range,x_range],[y_list_before,y_list_after,native_amino_freq],color=["darkmagenta", "mediumblue","black"], alpha=[0.9, 0.9,0.5], line_width=3)
+#p.multi_line([x_range,x_range],[y_list_before,y_list_after],color=["darkmagenta", "mediumblue"], alpha=[0.9, 0.9], line_width=3, legend=["before",'after'])
 
+p.line(x_range,y_list_before,color="darkmagenta",alpha=0.9,line_width=3,legend='before')
+p.line(x_range,y_list_after,color="mediumblue",alpha=0.9,line_width=3,legend='after')
+p.line(x_range,native_amino_freq,color="black",alpha=0.9,line_dash=[6,3],line_width=3,legend='native')
+#p.legend.orientation = "left_top"
+p.legend.location = "top_right"
+p.legend.orientation="horizontal"
+p.yaxis.axis_label = "Frequency"
+p.xaxis.axis_label = "Amino Acid"
+p.xaxis.major_label_text_font_size='12pt'
+p.yaxis.major_label_text_font_size='12pt'
+p.yaxis.axis_label_text_font_size='13pt'
+p.xaxis.axis_label_text_font_size='13pt'
 show(p)
